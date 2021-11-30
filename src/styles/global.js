e66e619ae8c4a054e5f498b58c2886baa8afc91a
IgorFMouta <89466547+IgorFMouta@users.noreply.github.com>
2021-11-30 20:51:56 -0300

    Igor  30/11/21 20:51 - Commit Inicial

---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle `
+    body {
+        background-image: url("https://github.com/RBaltar/Ecommerce/blob/main/Background%20Login.png?raw=true");
+        background-repeat:no-repeat;
+        background-size: 100%;
+        background-color: #808080;
+    }
+`
---
+++ src/styles/index.js
@@ -0,0 +1,8 @@
+import { Button } from "./styles";
+
+function PrimaryButton({ message }) {
+    return (
+        <Button>{message}</Button>
+    )
+}
+export default PrimaryButton;
